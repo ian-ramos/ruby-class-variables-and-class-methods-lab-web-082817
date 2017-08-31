04d7ca04022c1735d3ee3df442184e6668fb4ac2
ian-ramos <ramos.ianc@gmail.com>
2017-08-30 22:15:18 -0400

    Done

---
+++ lib/run.rb
@@ -0,0 +1,6 @@
+require_relative './song'
+require 'pry'
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+thriller = Song.new("Thriller", "Michael Jackson", "pop")
+Song.genre_count
